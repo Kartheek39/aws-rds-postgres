3603b221e70d99a9e9958ccdc54af2953dc3d7c7
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2024-01-02 18:19:54 +0530

    Update test.py

--- sql-scripts/test.py
+++ sql-scripts/test.py
@@ -16,9 +16,13 @@ def create_pagila_db():
         global conn
         with conn:
             with conn.cursor() as curs:
-                curs.execute(open("../sql-scripts/schema/schema.sql", "r").read())
+                curs.execute(open("../sql-scripts/test.sql", "r").read())
                 #curs.execute(open("../sql-scripts/data.sql", "r").read())
                 conn.commit()
                 print('Pagila SQL scripts executed')
+    except (psycopg2.OperationalError, psycopg2.DatabaseError, FileNotFoundError) as err:
+        print(create_pagila_db.__name__, err)
+        close_conn()
+        exit(1)
   
 
