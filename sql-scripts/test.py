f250560ffc3f2c3dbbb958a61736b05ee60fab38
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2024-01-02 18:29:27 +0530

    Update test.py

--- sql-scripts/test.py
+++ sql-scripts/test.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 
-import argparse
-import psycopg2
+print('Hellow World')
 
-from db_config import config
 
-conn = None  # global variable
+# import argparse
+# import psycopg2
 
-def create_pagila_db():
-    """
-    Creates Pagila database by running DDL and DML scripts
-    """
+# from db_config import config
 
-    try:
-        global conn
-        with conn:
-            with conn.cursor() as curs:
-                curs.execute(open("../sql-scripts/test.sql", "r").read())
-                #curs.execute(open("../sql-scripts/data.sql", "r").read())
-                conn.commit()
-                print('Pagila SQL scripts executed')
-    except (psycopg2.OperationalError, psycopg2.DatabaseError, FileNotFoundError) as err:
-        print(create_pagila_db.__name__, err)
-        close_conn()
-        exit(1)
+# conn = None  # global variable
+
+# def create_pagila_db():
+#     """
+#     Creates Pagila database by running DDL and DML scripts
+#     """
+
+#     try:
+#         global conn
+#         with conn:
+#             with conn.cursor() as curs:
+#                 curs.execute(open("../sql-scripts/test.sql", "r").read())
+#                 #curs.execute(open("../sql-scripts/data.sql", "r").read())
+#                 conn.commit()
+#                 print('Pagila SQL scripts executed')
+#     except (psycopg2.OperationalError, psycopg2.DatabaseError, FileNotFoundError) as err:
+#         print(create_pagila_db.__name__, err)
+#         close_conn()
+#         exit(1)
   
 
