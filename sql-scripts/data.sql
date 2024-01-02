211ac130b5fb9d16bee0e18f492c9c053a8df667
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2024-01-02 16:20:46 +0530

    Update data.sql

--- sql-scripts/data.sql
+++ sql-scripts/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO public.kartheek1
 (id, "name", address1, address2, city)
 select 3,'mahesh','abc','def','hyd';
+
+INSERT INTO public.kartheek1
+(id, "name", address1, address2, city)
+select 3,'kartheek','abc','def','alwal';
