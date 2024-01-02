89305a9e2432497dabb20f4aa0a0d41cd75e3f36
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2024-01-02 17:46:12 +0530

    Update test.sql

--- sql-scripts/test.sql
+++ sql-scripts/test.sql
@@ -1,7 +1,3 @@
-create table kartheek1 (
-    id int primary key,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
+#!/usr/bin/env python3
+
+curs.execute(open("../sql-scripts/schema/schema.sql", "r").read())
