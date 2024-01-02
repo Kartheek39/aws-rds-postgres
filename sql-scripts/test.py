cfb964b529556a84e481c5c21620d4850fc18b5b
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2024-01-02 18:05:51 +0530

    Update test.py

--- sql-scripts/test.py
+++ sql-scripts/test.py
@@ -3,6 +3,22 @@
 import argparse
 import psycopg2
 
-curs.execute(open("../sql-scripts/test.sql", "r").read())
+from db_config import config
+
+conn = None  # global variable
+
+def create_pagila_db():
+    """
+    Creates Pagila database by running DDL and DML scripts
+    """
+
+    try:
+        global conn
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute(open("../sql-scripts/schema/schema.sql", "r").read())
+                #curs.execute(open("../sql-scripts/data.sql", "r").read())
+                conn.commit()
+                print('Pagila SQL scripts executed')
   
 
